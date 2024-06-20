5716a00024b19133d59a80117719de46f200c863
RLungWu <larry14789@gmail.com>
2024-06-20 23:36:44 +0800

    Bug fixed

--- include/container/vector.h
+++ include/container/vector.h
@@ -1,6 +1,7 @@
 #ifndef STL_MOCK_INCLUDE_CONTAINER_VECTOR_H_
 #define STL_MOCK_INCLUDE_CONTAINER_VECTOR_H_
 
+#include <cstddef>
 
 #include "allocator.h"
 
@@ -10,9 +11,28 @@ namespace mystl {
     template <typename T, class Alloc = Allocator<T>>
     class MyVector{
         public:
-            MyVector();
-            MyVector(int size);
-        private:
+            //vector type
+            typedef T value_type;
+            typedef value_type* pointer;
+            typedef value_type& reference;
+            typedef value_type* iterator;
+            typedef std::size_t size_type;
+            typedef std::ptrdiff_t difference_type;
+        
+        protected:
+            typedef simple_alloc<value_type, Alloc> data_allocator;
+            iterator start; //The memory address of the first element
+            iterator finish; //The memory address of the last element
+            iterator end_of_storage; //The memory address of the end of the storage
+
+            void insert_aux(iterator position, const T& x);
+            void deallocate(){
+                if(start){
+                    data_allocator::deallocate(start, end_of_storage - start);
+                }
+            }
+        
+
     };
 }
 
